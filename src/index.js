7974a75bcdb66636f75f728334a9bae3147f6788
Tyler Kirumba <Tyler>
2023-03-25 20:59:50 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    buildTaskDes(e.target.new_task_description.value)
+    form.reset();
+
+  })
 });
+
+function buildTaskDes(taskDes){
+  console.log(taskDes)
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', function handleDelet(e){
+    e.target.parentNode.remove()
+  })
+  btn.textContent = 'x'
+  p.textContent = taskDes + " "
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
